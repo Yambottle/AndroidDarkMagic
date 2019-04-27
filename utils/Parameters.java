54050b0b121bf7903dfea5dfcc7343be5f66b52e
Yambottle <31813282+Yambottle@users.noreply.github.com>
2019-04-27 12:16:44 -0500

    Update Parameters.java

--- utils/Parameters.java
+++ utils/Parameters.java
@@ -25,6 +25,16 @@ public class Parameters implements Serializable {
         StatusBarCompat.setStatusBarColor(activity, ContextCompat.getColor(context, color));
     }
 
+    public static void setStatusbarTransparent(Activity activity){
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = activity.getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+    
     public static int getStatusbarHeight(Window window) {
         Rect rectangle = new Rect();
         window.getDecorView().getWindowVisibleDisplayFrame(rectangle);
